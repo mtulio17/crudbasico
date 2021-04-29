a6d62a74d76d6a3a015551a2df3343029190a100
tulio <tuliomoya17@gmail.com>
2021-04-29 17:36:37 -0300

    los datos se cargan en el formulario de editar

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Container, Form, Button, Alert } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import { useParams } from 'react-router-dom';
@@ -7,7 +7,15 @@ const EditarProducto = () => {
 
     //const id= useParams().id
     const { id } = useParams();
+    
+
+    //variables useRef
+    const nombreProductoRef = useRef('');
+    const precioProductoRef = useRef(0);
+    //creo los state
     const [producto, setProducto] = useState({});
+    const [categoria, setCategoria] = useState();
+
 
 
     useEffect(() => {
@@ -32,7 +40,7 @@ const EditarProducto = () => {
     };
 
     const cambioCategoria = (e) => {
-        //setCategoria(e.target.value)
+        setCategoria(e.target.value)
     };
 
     const handleSubmit = () => { };
@@ -43,19 +51,19 @@ const EditarProducto = () => {
                 <h1 className='text-center my-5'>Editar producto</h1>
                 <Form.Group>
                     <Form.Label>Nombre del Producto*</Form.Label>
-                    <Form.Control type='text' placeholder='Té'></Form.Control>
+                    <Form.Control type='text' placeholder='Té' ref={nombreProductoRef} defaultValue={producto.nombreProducto}></Form.Control>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Precio*</Form.Label>
-                    <Form.Control type='Number' placeholder='0'></Form.Control>
+                    <Form.Control type='Number' placeholder='0' ref={precioProductoRef} defaultValue={producto.precioProducto}></Form.Control>
                 </Form.Group>
 
                 <h3 className='text-center my-4'>Categoria</h3>
                 <div className='text-center'>
-                    <Form.Check type='radio' name='categoria' inline label='Bebidas calientes' value='bebidaCaliente' onChange={cambioCategoria}></Form.Check>
-                    <Form.Check type='radio' name='categoria' inline label='Bebidas frias' value='bebidaFria' onChange={cambioCategoria}></Form.Check>
-                    <Form.Check type='radio' name='categoria' inline label='Dulce' value='dulce' onChange={cambioCategoria}></Form.Check>
-                    <Form.Check type='radio' name='categoria' inline label='Salado' value='salado' onChange={cambioCategoria}></Form.Check>
+                    <Form.Check type='radio' name='categoria' inline label='Bebidas calientes' value='bebidaCaliente' onChange={cambioCategoria} defaultChecked={producto.categoria && producto.categoria === 'bebidaCaliente'}></Form.Check>
+                    <Form.Check type='radio' name='categoria' inline label='Bebidas frias' value='bebidaFria' onChange={cambioCategoria} defaultChecked={producto.categoria && producto.categoria === 'bebidaFria'}></Form.Check>
+                    <Form.Check type='radio' name='categoria' inline label='Dulce' value='dulce' onChange={cambioCategoria}defaultChecked={producto.categoria && producto.categoria === 'dulce'}></Form.Check>
+                    <Form.Check type='radio' name='categoria' inline label='Salado' value='salado' onChange={cambioCategoria} defaultChecked={producto.categoria && producto.categoria === 'salado'}></Form.Check>
                 </div>
                 <Button variant='danger' type='submit' className='w-100 my-5'>Guardar</Button>
                 {/* {
